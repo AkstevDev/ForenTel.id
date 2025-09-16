407962430a87274367d54df440e16a23b5d7155c
AkstevDev <akstev.dev@gmail.com>
2025-09-16 10:07:42 +0700

    Create Apppage.tsx

---
+++ app/Apppage.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>ForenTel 🚀</h1>
+      <p>Website berhasil di-deploy ke Vercel.</p>
+    </main>
+  );
+}
